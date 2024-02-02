2b6c117543d785e551836f57a127612bd2013ce4
vbsunupe <vbs121996@gmail.com>
2024-02-02 13:19:10 +0530

    adding new project

---
+++ AutomationProject/src/test/java/Magneto_pageobjects/MagnetoHomepage.java
@@ -0,0 +1,30 @@
+package Magneto_pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class MagnetoHomepage{
+
+	WebDriver driver;
+
+	public MagnetoHomepage(WebDriver driver) {
+
+		this.driver = driver;
+		PageFactory.initElements(driver,this);
+
+	}
+	@FindBy(xpath="//div[@class='panel header']//a[normalize-space()='Create an Account']")
+	WebElement CreateAnaccount;
+	
+
+	public WebElement CreateAnaccount() {
+
+		return CreateAnaccount;
+	}
+	
+	
+}
+//a[@class="logo"]
